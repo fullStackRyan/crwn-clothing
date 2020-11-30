4d0774d1be5896ca7842a4593e0277ea5317944e
Ryan <ryan.mcavoy@itv.com>
2020-11-30 20:54:14 +0000

    add collection page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
-
+import ShopPage from './pages/shop/shop.component'
 import HomePage from './pages/homepage/homepage.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path='/' component={HomePage}/>
+                <Route path='/shop' component={ShopPage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
